fca1cdd01bea7089ea1782dce176a6b7659f3893
Mendrika Randriamahaleo <mendrikaandriamitantsoa@gmail.com>
2025-05-14 23:30:52 +0300

    My firs commit "mesdocs"

---
+++ Application/Interfaces/IUserServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mesdoc.Application.DTOs;
+
+namespace mesdoc.Application.Interfaces
+{
+    public interface IUserServices
+    {   
+        public List<User> GetUsers();
+    }
+}
---
+++ Application/Services/UserServices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mesdoc.Application.DTOs;
+using mesdoc.Application.Interfaces;
+
+namespace mesdoc.Application.Services
+{
+    public class UserServices: IUserServices
+    {
+        public List<User> GetUsers()
+        {
+            List<User> users = new List<User>();
+            users.Add(new User { Id = 1, Name = "John", FirstName = "Doe", Age = 30 });
+            return users;
+        }
+    }
+}
